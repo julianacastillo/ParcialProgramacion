1d66bf78f424df483d5a059830b7deb754dbe8be
Familia <Familia@DESKTOP-ASKVB46.home>
2020-11-26 11:30:05 -0500

    PARCIAL

---
+++ src/parcial/main.java
@@ -0,0 +1,16 @@
+
+package parcial;
+
+    /**
+     * @autor: JulianaCastillo
+     * II Semestre
+     * Parcial Final Programacion I
+     */   
+public class main {
+   public static void main (String [] args){
+       LecheChocolate objeto = new LecheChocolate ();
+       LecheFresa objetoFresa = new LecheFresa ();
+       objeto.imprimirLecheChocolate();
+       objetoFresa.imprimirLecheFresa();
+   }
+}
